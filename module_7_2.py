2386aa7a36bf6540b7d023c4e749afe325726756
akbay74 <asv1974@list.ru>
2024-09-27 18:42:37 +0400

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -2,7 +2,7 @@ def custom_write(file_name, strings):
     file = open(file_name, 'w', encoding='utf-8')
     strings_positions = {}
     for str in strings:
-        key_str = (strings.index(str), file.tell())
+        key_str = (strings.index(str) + 1, file.tell())
         strings_positions[key_str] = str
         file.write(str + '\n')
     file.close()
