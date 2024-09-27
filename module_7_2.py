84329a039a0d1aa859018297363f49e5e58dc9c4
akbay74 <asv1974@list.ru>
2024-09-27 18:41:16 +0400

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,20 @@
+def custom_write(file_name, strings):
+    file = open(file_name, 'w', encoding='utf-8')
+    strings_positions = {}
+    for str in strings:
+        key_str = (strings.index(str), file.tell())
+        strings_positions[key_str] = str
+        file.write(str + '\n')
+    file.close()
+    return strings_positions
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
